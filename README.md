9d61526f2ba194ed7f7773014a5b992eebb9d333
Beatriz <120328207+xbeatriz@users.noreply.github.com>
2025-02-20 19:29:39 +0000

    correct files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-study-2
+# vue-study-4
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -4,88 +4,39 @@
   <p v-else-if="status === 'inactive'">User is inactive</p>
   <p v-else>User is pending</p>
 
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask">
-    <button type="submit">Submit</button>
-  </form>
-
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>
-        {{ task }}
-      </span>
-      <button @click="deleteTask(index)">X</button>
-    </li>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <br>
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>
 </template>
 
-
-
-<script setup>
-//? SMALL VERSION
-import { ref } from 'vue';  // ref is a function that creates a reactive reference
-
-const name = ref('John Doe');
-const status = ref('active');
-const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
-const newTask = ref('');
-
-const toggleStatus = () => {
-  if (status.value === 'active') {
-    status.value = 'pending';
-  } else if (status.value === 'pending') {
-    status.value = 'inactive';
-  } else {
-    status.value = 'active';
-  }
-}
-
-const addTask = () => {
-  if (newTask.value.trim() != '') {
-    tasks.value.push(newTask.value);
-    newTask.value = '';
-  }
-}
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-}
-
-
-</script>
-<!-- <script>
-//? LARGE VERSION
-import { ref } from 'vue';  // ref is a function that creates a reactive reference
-
+<script>
 export default {
-  setup() {
-    const name = ref('John Doe');
-    const status = ref('active');
-    const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
-
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
+  data() {
+    return {
+      name: 'Jonh doe',
+      status: 'active',
+      tasks: ['task 1', 'task 2', 'task 3'],
+      link: 'https://www.google.com',
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
       } else {
-        status.value = 'active';
+        this.status = 'active';
       }
     }
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus
-    } 
-  }
-} 
+  },
+};
 </script>
--->
 
-<style></style>
+
